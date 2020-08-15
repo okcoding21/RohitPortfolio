9a520d2f9f6c29efbe311c6c80cfe4ab79a2268f
okcoding21 <rohit.asawa21@gmail.com>
2020-08-15 13:27:38 +0530

    C

---
+++ precache-manifest.b72a8b84d9c697d8b576719020aaedbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e25a7db69ef73323637c7b0f79a382c",
+    "url": "/RohitPortfolio/index.html"
+  },
+  {
+    "revision": "ebf3302161a8353bfe64",
+    "url": "/RohitPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "059e318490b14f9fbaf9",
+    "url": "/RohitPortfolio/static/css/main.051650a8.chunk.css"
+  },
+  {
+    "revision": "ebf3302161a8353bfe64",
+    "url": "/RohitPortfolio/static/js/2.c2c237fe.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/RohitPortfolio/static/js/2.c2c237fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059e318490b14f9fbaf9",
+    "url": "/RohitPortfolio/static/js/main.d0a9e9e5.chunk.js"
+  },
+  {
+    "revision": "a33fb63d5d6565072291",
+    "url": "/RohitPortfolio/static/js/runtime-main.30e3a2de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RohitPortfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bbd9d6bf2109248dde4a4052521f0195",
+    "url": "/RohitPortfolio/static/media/pp.bbd9d6bf.jpg"
+  }
+]);
